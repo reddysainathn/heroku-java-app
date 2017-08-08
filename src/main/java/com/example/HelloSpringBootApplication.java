9dacfe1c1b64524cff13a271655cb4add455b392
Narahari, Sainath <sainath.narahari@ge.com>
2017-08-08 00:48:19 -0500

    Checking automatic Builds with Heroku

--- src/main/java/com/example/HelloSpringBootApplication.java
+++ src/main/java/com/example/HelloSpringBootApplication.java
@@ -13,7 +13,7 @@ public class HelloSpringBootApplication {
     @RequestMapping("/")
     @ResponseBody
     String sayHello() {
-        return "hello, world";
+        return "Under Construction";
     }
 
     public static void main(String[] args) {
